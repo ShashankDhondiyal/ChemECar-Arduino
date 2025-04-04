b8c3b5bea7ac62e7810d731dc12322a7e18d110e
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-04-04 15:34:19 -0400

    Updated ThermoCouple.cc

--- ThermoCouple.cc
+++ ThermoCouple.cc
@@ -1,6 +1,6 @@
-#include <max6675.h>
 // https://randomnerdtutorials.com/arduino-k-type-thermocouple-max6675/
 
+#include <max6675.h>
 int thermoDO = 4;
 int thermoCS = 5;
 int thermoCLK = 6;
@@ -24,15 +24,22 @@ void setup() {
 void loop() {
     for (int minutes = 0; minutes < 60; minutes++) {
         for (double seconds = 0.00; seconds < 60.00; seconds+= 0.25) {
-            
             if (thermocouple.readCelsius() == criticalValue){
                 digitalWrite(RELAY, OFF);
-                Serial.println(minutes + ":" + seconds + ", " + thermocouple.readCelsius() + "    ON -> OFF");
+                Serial.print(minutes);
+                Serial.print(":");
+                Serial.print(seconds);
+                Serial.print(", switch ");
+                Serial.println(thermocouple.readCelsius());
             }
-            else{
-                Serial.println(minutes + ":" + seconds + ", " + thermocouple.readCelsius());
+            else {
+                Serial.print(minutes);
+                Serial.print(":");
+                Serial.print(seconds);
+                Serial.print(", ");
+                Serial.println(thermocouple.readCelsius());
             }
+            delay(250);
         }
     }
-    
 }
