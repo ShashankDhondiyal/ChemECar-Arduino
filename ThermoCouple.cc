e0caf35e1c2e0478a7eb90cbdf1afef9cb3d3f13
Shashank Dhondiyal <163368590+ShashankDhondiyal@users.noreply.github.com>
2025-04-11 02:33:23 -0400

    Updated ThermoCouple.cc
    refactored program

--- ThermoCouple.cc
+++ ThermoCouple.cc
@@ -1,45 +1,47 @@
-// https://randomnerdtutorials.com/arduino-k-type-thermocouple-max6675/
-
 #include <max6675.h>
-int thermoDO = 4;
-int thermoCS = 5;
-int thermoCLK = 6;
-const int RELAY = 3;
-int criticalValue = 0;
 
-#define ON  HIGH
-#define OFF LOW
-
-MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
+const int THERMO_DO = 4;
+const int THERMO_CS = 5;
+const int THERMO_CLK = 6;
+const int RELAY_PIN = 3;
+const float CRITICAL_TEMP = 50.0;
+const float TEMP_THRESHOLD = 0.5;
+const unsigned long SAMPLE_INTERVAL = 250;
+MAX6675 thermocouple(THERMO_CLK, THERMO_CS, THERMO_DO);
+unsigned long previousMillis = 0;
 
 void setup() {
-
     Serial.begin(9600);
-    Serial.println("MAX6675 init");
+    Serial.println("MAX6675 initializing");
+    pinMode(RELAY_PIN, OUTPUT);
+    digitalWrite(RELAY_PIN, HIGH);
     delay(500);
-    pinMode(RELAY, OUTPUT);
-
+    Serial.println("__________Ready__________");
 }
 
 void loop() {
-    for (int minutes = 0; minutes < 60; minutes++) {
-        for (double seconds = 0.00; seconds < 60.00; seconds+= 0.25) {
-            if (thermocouple.readCelsius() == criticalValue){
-                digitalWrite(RELAY, OFF);
-                Serial.print(minutes);
-                Serial.print(":");
-                Serial.print(seconds);
-                Serial.print(", switch ");
-                Serial.println(thermocouple.readCelsius());
-            }
-            else {
-                Serial.print(minutes);
-                Serial.print(":");
-                Serial.print(seconds);
-                Serial.print(", ");
-                Serial.println(thermocouple.readCelsius());
-            }
-            delay(250);
-        }
+    unsigned long currentMillis = millis();
+    
+    if (currentMillis - previousMillis >= SAMPLE_INTERVAL) {
+        previousMillis = currentMillis;
+        
+        unsigned long totalSeconds = currentMillis / 1000;
+        int minutes = (totalSeconds / 60) % 60;
+        int seconds = totalSeconds % 60;
+        
+        float temperature = thermocouple.readCelsius();
+        
+        Serial.print(minutes);
+        Serial.print(":");
+        if (seconds < 10) Serial.print("0");
+        Serial.print(seconds);
+        Serial.print(", ");
+        
+        bool isNearCritical = abs(temperature - CRITICAL_TEMP) < TEMP_THRESHOLD;
+        digitalWrite(RELAY_PIN, isNearCritical ? LOW : HIGH);
+        isNearCritical ? Serial.print(F("**___Switched OFF___**")) : 0;
+        
+        Serial.print("Temp: ");
+        Serial.println(temperature);
     }
 }
